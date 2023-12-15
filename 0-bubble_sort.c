1835e1a72b4cab3509892347fee4f5bd0bdb5ea2
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-15 03:55:05 -0800

    Adding repor

---
+++ 0-bubble_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - implementing bubble sort algorithm.
+ * @array: array to be sorted.
+ * @size: size of the array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, swapped;
+
+	j = 0;
+	while (j < size - 1)
+	{
+		swapped = 0;
+		for (i = 0; i < size - 1 - j; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped = 1;
+			}
+		}
+		print_array(array, size);
+		if (swapped == 0)
+			break;
+		j++;
+	}
+}
