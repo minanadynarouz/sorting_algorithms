c9d27ac11d0930a2bf39ced12fa618655fb14889
minanadynarouz <mina.n.narouz@gmail.com>
2023-12-15 04:10:53 -0800

    amend

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,7 +18,7 @@ void bubble_sort(int *array, size_t size)
 	while (j < size - 1)
 	{
 		swapped = 0;
-		for (i = 0; i < size - 1 - j; i++)
+		for (i = 0; i < size - j - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
